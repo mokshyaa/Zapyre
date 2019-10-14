5e26ea90b50e95cce94a002e2735631dcefda79a
Moksh <hardik.parekh@green-apex.com>
2019-10-14 13:23:19 +0530

    compress code by removing unnessesary loops

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,28 +1,18 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :products 
-  accepts_nested_attributes_for :products
-  after_initialize :get_quantity
   before_save :order_total 
-	#	before_save :set_product , on: :create
 
   private
-
-  #def set_product
-  	#@product = Product.find(params[:product_id])
-  #end
-
-  #params = {order: { products_attributes: [ :product_id ]}
-	#	}
+ 
    def order_total
-   	@products = Product.all
-   	@carts = Cart.all
-   	@orders = Order.all
+ 
     	@products.each do |product|
   	 	  product.orders.each do |order| 
   			  @orders.each do |order_main| 
 		  			if order.id == order_main.id						  				
-		  				order_main.total = product.price * @get_quantity 
+		  				order_main.total = product.price * @get_quantity
+		  				order.total = order_main.total
 		  			end
 		  		end
 		  	end
@@ -30,21 +20,7 @@ class Order < ApplicationRecord
 		end
 	end
 
-		def get_quantity
-		 	@products = Product.all
-	   	@carts = Cart.all
-	   	@orders = Order.all
-	   	@get_quantity = []
-		  				 	@products.each do |product|
-		  				 		product.carts.each do |cart|
-		  				 			@carts.each do |cart_main|
-		  				 				if cart.id == cart_main.id
-		  				 					@get_quantity = cart.quantity
-		  				 				end
-		  				 			end
-		  				 		end
-		  				 	end
-		  				end
+
 
 
 
