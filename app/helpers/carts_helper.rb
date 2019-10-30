89559983b9725543b145d05b40bc3be43bb7781a
Moksh <moksh.panchal@green-apex.com>
2019-10-30 10:37:56 +0530

    update

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,16 +1,3 @@
 module CartsHelper
-#was trying to create helpers (custom form helpers)
 
-	#def buy
-	  #@carts.each do |cart|
-	   # if cart.user_id == current_user.id
-	  	#	 	cart.user_id
-  		#	  cart.quantity
-		  #   form_with(model:  @order,  url: orders_path, method: "post", local: true) do |form| 
-			#  	form.hidden_field :quantity, value: cart.quantity 
-			#	  form.submit 'Buy'
-			 # end
-		 # end
-	  #end
-  #end
 end
